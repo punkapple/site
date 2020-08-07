dc285b4f8ab93e5e5ef2e7246b39cbb853f09665
Владимир Севрюков <IT-3@ad.rcz-dnr.ru>
2020-08-07 13:26:33 +0300

    Laravel Uikit3 starter

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+@section('content')
+        <div class="uk-flex uk-flex-middle uk-flex-center" uk-height-viewport="expand: true">
+            <div class="uk-container uk-section">
+                <div class="">
+           <h1 class="uk-heading-medium uk-text-center uk-text-light">Laravel</h1>
+                </div>
+                <div class="links">
+                    <a class="uk-text-large uk-text-light uk-padding-small uk-link-heading" href="https://laravel.com/docs">Docs</a>
+                    <a class="uk-text-large uk-text-light uk-padding-small uk-link-heading" href="https://laracasts.com">Laracasts</a>
+                    <a class="uk-text-large uk-text-light uk-padding-small uk-link-heading" href="https://laravel-news.com">News</a>
+                    <a class="uk-text-large uk-text-light uk-padding-small uk-link-heading" href="https://blog.laravel.com">Blog</a>
+                    <a class="uk-text-large uk-text-light uk-padding-small uk-link-heading" href="https://nova.laravel.com">Nova</a>
+                    <a class="uk-text-large uk-text-light uk-padding-small uk-link-heading" href="https://forge.laravel.com">Forge</a>
+                    <a class="uk-text-large uk-text-light uk-padding-small uk-link-heading" href="https://vapor.laravel.com">Vapor</a>
+                    <a class="uk-text-large uk-text-light uk-padding-small uk-link-heading" href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
+            </div>
+        </div>
+@endsection
